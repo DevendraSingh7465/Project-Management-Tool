50ef939bdcd749732a0fb5ca0a41b8cb46f200c7
Devendra Singh <127510525+DevendraSingh7465@users.noreply.github.com>
2024-03-21 19:28:05 +0530

    added both the cases select and deselect.

--- status.php
+++ status.php
@@ -1,29 +1,24 @@
 <?php
-// session is important for sharing varaible data from oen file to  another
-// session_start(); 
+include("connection.php");
 error_reporting(E_ERROR | E_PARSE);
 
+//get data from status_check() function
 $project_name = $_POST['postProjectName'];
 $task_name = $_POST['postTaskName'];
-// $task_name = filter_input(INPUT_POST, 'task_name');
-// echo $projectName;
+$status_check = $_POST['statusCheck'];
 
-
-//For PHP MyAdmin
-$host = "localhost:3308";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "pmt";
-
-// Create connection
-$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-
- 
-$sql = "UPDATE notes SET status='1' WHERE projects='$project_name' and tasks='$task_name'";
-// $sql1 = "INSERT INTO projects (projectname) VALUES ('$project_name');";
-if (mysqli_query($conn, $sql)) {
-    // header("Location:index.php");
-} 
+if($status_check==0){
+    $sql = "UPDATE notes SET status='0' WHERE projects='$project_name' and tasks='$task_name'";
+    if (mysqli_query($conn, $sql)) {
+        // header("Location:index.php");
+    } 
+}
+else{
+    $sql = "UPDATE notes SET status='1' WHERE projects='$project_name' and tasks='$task_name'";
+    if (mysqli_query($conn, $sql)) {
+        // header("Location:index.php");
+    } 
+}
 
 $TaskStatus = array();
 $sql1 = "select tasks from notes where projects='$project_name' and status = '1'";
@@ -31,10 +26,6 @@ $query1 = mysqli_query($conn,$sql1);
 while($row = mysqli_fetch_array($query1)){
     $doneTasks = $row['tasks'];
     array_push($TaskStatus,$doneTasks);
-    // echo $doneTasks;
 }
-echo json_encode($TaskStatus); // Encode the data as JSON
-
-
-
+echo json_encode($TaskStatus); 
 ?>
