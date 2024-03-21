9e22f2e762b23d429a26f12d5c5167864554e625
Devendra Singh <127510525+DevendraSingh7465@users.noreply.github.com>
2024-03-21 19:20:07 +0530

    added comment and reduced LOC

--- index.php
+++ index.php
@@ -9,53 +9,41 @@
     <title>Project Management Tool</title>
     <link rel="stylesheet" href="style.css">
 	<script src="jquery-3.7.1.min.js"></script>
-
 </head>
 <body>
     <div class="outer">
         <div class="add-project-box" id="add-project-box">
             <form method="post" action="create_project.php" onsubmit="return validateForm()" name="createprojectform">
-                <label for="">Create Project</label><br />
+                <label for="input_project_name">Create Project</label><br />
                 <input type="text" name="project_name" id="input_project_name" placeholder="Enter Project Name..." /><br />
-                <button onclick="close_add_project_box()" type="reset">CANCEL</button>
-                <button type="submit">CREATE</button>
+                <button onclick="close_add_project_box()" type="reset" class="cancel_btn_">CANCEL</button>
+                <button type="submit" class="create_btn_">CREATE</button>
             </form>
         </div>
         <div class="add-task-box" id="add-task-box">
-            
             <label for="">Create Task</label><br />
             <input type="text" name="task_name" id="input_task_name" placeholder="Enter Task Name..." /><br />
-            <button onclick="close_add_task_box()" type="reset">CANCEL</button>
-            <button  onclick="postProjectName()">CREATE</button>
-            
+            <button onclick="close_add_task_box()" type="reset" class="cancel_btn_">CANCEL</button>
+            <button  onclick="postProjectName()" class="create_btn_">CREATE</button>
         </div>
         <div class="left-box">
             <h1>PROJECTS</h1>
-
             <div class="all-projects">
                 <div id="link_wrapper"></div>
             </div>
-
             <button id="myBtn" class="create-project-btn" onclick="create_project()">CREATE PROJECT</button>
-            
-
         </div>
         <div class="right-box">
             <h1>TASKS</h1>
-
             <div class="all-tasks">
                 <div id="link_wrapper1"></div>
             </div>
-
             <button class="create-task-btn" id="create-task-btn" onclick="Create_task_()">
                 <img class="plus" src="white-plus.png" alt="">
             </button>
-            
         </div>
-
     </div>
     <script src="script.js"></script>
-    
 </body>
 </html>
 
@@ -74,26 +62,17 @@
     }
     loadXMLDoc();
     
-        
     function loadXMLDoc1() {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("link_wrapper1").innerHTML =
-            this.responseText;
-        }
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("link_wrapper1").innerHTML =
+                this.responseText;
+            }
         };
         xhttp.open("GET", "server2.php", true);
         xhttp.send();
     }
     loadXMLDoc1();
-    // setInterval(function(){
-    //         loadXMLDoc();
-    //         loadXMLDoc1();
-    //         // 1sec
-    // },500);
-
     window.onload = loadXMLDoc;
-        
-    
 </script>
