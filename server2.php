c4e6b7e628491805cafb1e8481c86c44151d6e32
Devendra Singh <127510525+DevendraSingh7465@users.noreply.github.com>
2024-03-21 19:23:19 +0530

    Update server2.php

--- server2.php
+++ server2.php
@@ -1,13 +1,13 @@
+<!-- This File fetch the tasks from the database and show it on .all-tasks class element which is right part of the app. -->
 <?php 
+	error_reporting(E_ERROR | E_PARSE);
 	include("connection.php");
+    $project_name = $_POST['postProjectName']; // gets data from showtasksrelatedtoproject()
 
  	$all_tasks_names = array();
-	error_reporting(E_ERROR | E_PARSE);
 
-    $project_name = $_POST['postProjectName'];
-
-    $sql = "SELECT tasks FROM notes WHERE projects = '$project_name'";
-	$query = mysqli_query($db, $sql);
+    $sql = "SELECT DISTINCT tasks FROM notes WHERE projects='$project_name' AND tasks IS NOT NULL AND tasks <> ''";
+	$query = mysqli_query($conn, $sql);
 	while($row = mysqli_fetch_array($query)){
 		$taskname = $row['tasks'];
 		array_push($all_tasks_names,$taskname);
@@ -20,9 +20,7 @@
         <input type='checkbox' id='$button_id' class='ui-checkbox' onclick='status_check()'>
         <label for='$button_id' class='table-btn12'>$record</label>
         </br>
-    </div>
-    ";
-					
+    </div>";				
 	}
 ?>
 
