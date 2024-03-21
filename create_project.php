b912321a0288a1a9d6ede89dcb85766b27a21c95
Devendra Singh <127510525+DevendraSingh7465@users.noreply.github.com>
2024-03-21 19:01:18 +0530

    added one comment and reduced the LOC

--- create_project.php
+++ create_project.php
@@ -1,26 +1,10 @@
+<!-- This file is used to create project -->
 <?php
-// session is important for sharing varaible data from oen file to  another
-session_start(); 
+include("connection.php");
 $project_name = filter_input(INPUT_POST, 'project_name');
 
-
-
-//For PHP MyAdmin
-$host = "localhost:3308";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "pmt";
-
-// Create connection
-$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-
-
 $sql = "INSERT INTO notes (projects) VALUES ('$project_name');";
-// $sql1 = "INSERT INTO projects (projectname) VALUES ('$project_name');";
 if (mysqli_query($conn, $sql)) {
     header("Location:index.php");
-
 } 
-
-
 ?>
