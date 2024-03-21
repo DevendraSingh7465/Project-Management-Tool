cb591cc2ee6059a480f79f11009961bb3b3790e9
Devendra Singh <127510525+DevendraSingh7465@users.noreply.github.com>
2024-03-21 19:18:39 +0530

    reduced LOC

--- create_task.php
+++ create_task.php
@@ -1,28 +1,13 @@
+<!-- This file is used to create task. -->
 <?php
-// session is important for sharing varaible data from oen file to  another
-// session_start(); 
+include("connection.php");
+
+// get data from postProjectName() function
 $project_name = $_POST['postProjectName'];
 $task_name = $_POST['postTaskName'];
-// $task_name = filter_input(INPUT_POST, 'task_name');
-// echo $projectName;
-
-
-//For PHP MyAdmin
-$host = "localhost:3308";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "pmt";
-
-// Create connection
-$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-
 
 $sql = "INSERT INTO notes (projects,tasks) VALUES ('$project_name','$task_name');";
-// $sql1 = "INSERT INTO projects (projectname) VALUES ('$project_name');";
 if (mysqli_query($conn, $sql)) {
     header("Location:index.php");
-
 } 
-
-
 ?>
